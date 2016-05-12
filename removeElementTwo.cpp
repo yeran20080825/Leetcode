92ff5c78c0aeef73c1353e927ef207ea54c87543
Aran <yeran20080825@qq.com>
2016-05-12 10:29:39 +0800

    Add removeElement

---
+++ removeElementTwo.cpp
@@ -0,0 +1,30 @@
+//
+// Created by Aran on 2016/05/12.
+//
+
+class Solution {
+public:
+    int removeElementTwo(vector<int>& nums, int val) {
+        int i=0;
+        int j=0;
+        for(i;i<nums.size();i++){
+            if(nums[i]==val){
+                continue;
+            }
+            nums[j]=nums[i];
+            j++;
+        }
+
+        return j;
+
+    }
+
+};
+int main() {
+
+    vector<int> num ={1,2,2,2,3,3};
+    Solution solution;
+
+    cout<<"the len of new array is "<<solution.removeElementTwo(num,6,2)<<endl;
+    return 0;
+}
